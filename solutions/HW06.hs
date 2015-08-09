ac509d88522470767a67f8b90224a09abd56d6c5
Amar Shah <amar@shareprogress.org>
2015-08-09 12:14:27 -0400

    Solve Homework 6, Exercise 2

--- solutions/HW06.hs
+++ solutions/HW06.hs
@@ -17,7 +17,7 @@ fibs1 = map fib [0..]
 -- Exercise 2 -----------------------------------------
 
 fibs2 :: [Integer]
-fibs2 = undefined
+fibs2 = 1 : 1 : zipWith (+) fibs2 (tail fibs2)
 
 -- Exercise 3 -----------------------------------------
 
