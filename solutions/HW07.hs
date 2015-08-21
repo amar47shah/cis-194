3f1ec3f0f2b1004432e5a31e609d5fedf419dc31
Amar Shah <amar@shareprogress.org>
2015-08-20 22:09:49 -0400

    Explore Homework 7, Exercise 5
    `randomSwapFirst` is a function that simply chooses a random element
    to swap out with the first element of a vector.

--- solutions/HW07.hs
+++ solutions/HW07.hs
@@ -63,6 +63,13 @@ randomVecR n = (V.fromList <$>) . replicateM n . getRandomR
 shuffle :: Vector a -> Rnd (Vector a)
 shuffle = undefined
 
+randomSwapFirst :: Vector a -> IO (Vector a)
+randomSwapFirst v = do
+  let n = V.length v
+  let i = 0
+  j <- evalRandIO $ getRandomR (0, pred n)
+  return $ v // [(i, v ! j), (j, v ! i)]
+
 -- Exercise 6 -----------------------------------------
 
 partitionAt :: Ord a => Vector a -> Int -> (Vector a, a, Vector a)
