ad1cac1e200b4648a5916ab0b221d5058f3f8fa1
Amar Shah <amar@shareprogress.org>
2015-09-15 12:14:48 -0400

    Implement tail recursive solution

--- 2013-byorgey/solutions/HW01.hs
+++ 2013-byorgey/solutions/HW01.hs
@@ -1,17 +1,18 @@
 module HW01 (toDigits, toDigitsRev) where
 
+--not tail-recursive
 toDigits :: Integer -> [Integer]
 toDigits = reverse . toDigitsRev
 
---not tail-recursive
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev n | n > 0 = n `mod` 10 : toDigitsRev (n `div` 10)
 toDigitsRev _         = []
 
-testToDigits :: Bool
-testToDigits = map toDigits [1234, 12, 0, -17] ==
-  [ [1, 2, 3, 4]
-  , [1, 2]
-  , []
-  , []
-  ]
+--tail-recursive
+toDigits' :: Integer -> [Integer]
+toDigits' n = go n []
+  where go n xs | n > 0 = go (n `div` 10) $ n `mod` 10 : xs
+        go _ xs         = xs
+
+test_digitize :: (Integer -> [Integer]) -> Bool
+test_digitize f = map f [1234, 12, 0, -17] == [[1,2,3,4], [1,2], [], []]
