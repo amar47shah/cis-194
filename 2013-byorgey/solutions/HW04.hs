cfc2c8dcca6e662b1baf93d2cf3cd425c792528b
Amar Shah <amar47shah@gmail.com>
2015-09-24 11:38:30 -0400

    Solve Homework 4, Exercise 2

--- 2013-byorgey/solutions/HW04.hs
+++ 2013-byorgey/solutions/HW04.hs
@@ -1,6 +1,10 @@
 {-# OPTIONS_GHC -Wall #-}
 module HW04 where
 
+--------------------------------------------------------------------------------
+
+-- Exercise 1
+
 fun1' :: [Integer] -> Integer
 fun1' = product . map (subtract 2) . filter even
 
@@ -21,3 +25,39 @@ fun2 1        = 0
 fun2 n
   | even n    = n + fun2 (n `div` 2)
   | otherwise =     fun2 (3 * n + 1)
+
+--------------------------------------------------------------------------------
+
+-- Exercise 2
+
+data Tree a = Leaf
+            | Node Integer (Tree a) a (Tree a)
+            deriving (Show, Eq)
+
+-- | Generates a balanced binary tree.
+foldTree :: [a] -> Tree a
+foldTree = foldr insert Leaf
+
+insert :: a -> Tree a -> Tree a
+insert x  Leaf                            = singleton x
+insert x (Node _ Leaf y Leaf)             = Node 1 (singleton x) y Leaf
+insert x (Node _ l@(Node _ _ _ _) y Leaf) = Node 1 l y (singleton x)
+insert x (Node _ Leaf y r@(Node _ _ _ _)) = Node 1 (singleton x) y r
+insert x (Node h l@(Node hl _ _ _) y r@(Node hr _ _ _))
+  | hl > hr    = Node h                 l  y r'
+  | hl < hr    = Node h                 l' y r
+  | hasSpace l = Node h                 l' y r
+  | hasSpace r = Node h                 l  y r'
+  | otherwise  = Node (max h (hl' + 1)) l' y r
+    where l'@(Node hl' _ _ _) = insert x l
+          r'                  = insert x r
+
+singleton :: a -> Tree a
+singleton x = Node 0 Leaf x Leaf
+
+-- | Determine whether a new node can be inserted without increasing height.
+hasSpace :: Tree a -> Bool
+hasSpace  Leaf                          = False
+hasSpace (Node _ Leaf _ (Node _ _ _ _)) = True
+hasSpace (Node _ (Node _ _ _ _) _ Leaf) = True
+hasSpace (Node _ l _ r)                 = hasSpace l || hasSpace r
--- 2013-byorgey/solutions/HW04Test.hs
+++ 2013-byorgey/solutions/HW04Test.hs
@@ -3,6 +3,7 @@ module HW04Tests where
 
 import HW04
 import Control.Arrow ((&&&))
+import Data.List (tails)
 
 import Test.Tasty
 import Test.Tasty.QuickCheck as QC
@@ -27,3 +28,39 @@ same f g = uncurry (==) . (f &&& g)
 
 smallPos :: Gen Integer
 smallPos = choose (1, 1000)
+
+-- TODO: Make these into unit tests!
+
+-- Verify that foldTree's results have correct heights, up to 257 elts.
+-- Should return True
+testHeightsCorrect :: Bool
+testHeightsCorrect = all (heightsCorrect . foldTree) $ tails ([1..257] :: [Integer])
+  where
+    heightsCorrect :: Tree a -> Bool
+    heightsCorrect Leaf                 = True
+    heightsCorrect (Node h Leaf _ Leaf) = h == 0
+    heightsCorrect (Node h Leaf _ (Node h' _ _ _)) = h == h' + 1
+    heightsCorrect (Node h (Node h' _ _ _) _ Leaf) = h == h' + 1
+    heightsCorrect (Node h l@(Node hl _ _ _) _ r@(Node hr _ _ _)) =
+      h == max hl hr + 1 && heightsCorrect l && heightsCorrect r
+
+-- Verify that height increases at 2^n elements.
+testBalancedInsert :: Bool
+testBalancedInsert = all (\(n, h) -> heightFor n == h) $
+                     [ (  0, -1)
+                     , (  1,  0)
+                     , (  2,  1), (  3,  1)
+                     , (  4,  2), (  7,  2)
+                     , (  8,  3), ( 15,  3)
+                     , ( 16,  4), ( 31,  4)
+                     , ( 32,  5), ( 63,  5)
+                     , ( 64,  6), (127,  6)
+                     , (128,  7), (255,  7)
+                     , (256,  8), (511,  8)
+                     , (512,  9)
+                     ]
+  where
+    height (Node h _ _ _) = h
+    height  Leaf          = -1
+    heightFor :: Integer -> Integer
+    heightFor n = height $ foldTree [1..n]
