bfdf02293796e62b191d2af0ad53fca9b1636d47
Amar Shah <amar47shah@gmail.com>
2015-09-23 20:47:43 -0400

    Add fun2 from assignment

--- 2013-byorgey/solutions/HW04.hs
+++ 2013-byorgey/solutions/HW04.hs
@@ -8,4 +8,10 @@ fun1 :: [Integer] -> Integer
 fun1 []      = 1
 fun1 (x:xs)
  | even x    = (x - 2) * fun1 xs
- | otherwise = fun1 xs
+ | otherwise =           fun1 xs
+
+fun2 :: Integer -> Integer
+fun2 1        = 0
+fun2 n
+  | even n    = n + fun2 (n ‘div‘ 2)
+  | otherwise =     fun2 (3 * n + 1)
