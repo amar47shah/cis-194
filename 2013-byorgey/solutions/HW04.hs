c45aa95313cf0febe3a0a3eeec6af8217e5ea830
Amar Shah <amar47shah@gmail.com>
2015-09-23 21:21:48 -0400

    Solve Homework 4, Exercise 1

--- 2013-byorgey/solutions/HW04.hs
+++ 2013-byorgey/solutions/HW04.hs
@@ -4,14 +4,20 @@ module HW04 where
 fun1' :: [Integer] -> Integer
 fun1' = product . map (subtract 2) . filter even
 
+fun2' :: Integer -> Integer
+fun2' = sum . filter even . takeWhile (/= 1) . iterate f
+  where f n | even n    = n `div` 2
+            | otherwise = 3 * n + 1
+
 fun1 :: [Integer] -> Integer
 fun1 []      = 1
 fun1 (x:xs)
  | even x    = (x - 2) * fun1 xs
  | otherwise =           fun1 xs
 
+-- WARNING: fun2 and fun2' produce infinite loops for input < 1
 fun2 :: Integer -> Integer
 fun2 1        = 0
 fun2 n
-  | even n    = n + fun2 (n ‘div‘ 2)
+  | even n    = n + fun2 (n `div` 2)
   | otherwise =     fun2 (3 * n + 1)
--- 2013-byorgey/solutions/HW04Test.hs
+++ 2013-byorgey/solutions/HW04Test.hs
@@ -13,7 +13,17 @@ main = defaultMain qcProps
 qcProps :: TestTree
 qcProps = testGroup "QuickCheck"
   [ QC.testProperty "fun1' == fun1" $ prop_sameFun1
+  , QC.testProperty "fun2' == fun2" $ forAll smallPos prop_sameFun2
   ]
 
 prop_sameFun1 :: [Integer] -> Bool
-prop_sameFun1 = uncurry (==) . (fun1 &&& fun1')
+prop_sameFun1 = same fun1 fun1'
+
+prop_sameFun2 :: Integer -> Bool
+prop_sameFun2 = same fun2 fun2'
+
+same :: Eq b => (a -> b) -> (a -> b) -> a -> Bool
+same f g = uncurry (==) . (f &&& g)
+
+smallPos :: Gen Integer
+smallPos = choose (1, 1000)
