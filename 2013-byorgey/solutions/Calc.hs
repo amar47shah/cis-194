42c30be1f054a266590bb5bad21e1c2b97278847
Amar Shah <amar47shah@gmail.com>
2015-09-29 09:26:59 -0400

    Solve Homework 5, Exercise 2

--- 2013-byorgey/solutions/Calc.hs
+++ 2013-byorgey/solutions/Calc.hs
@@ -2,6 +2,9 @@
 module Calc where
 
 import ExprT
+import Parser
+
+import Control.Monad (liftM)
 import Data.Function (on)
 
 --------------------------------------------------------------------------------
@@ -12,3 +15,10 @@ eval :: ExprT -> Integer
 eval (Lit x)   = x
 eval (Add x y) = ((+) `on` eval) x y
 eval (Mul x y) = ((*) `on` eval) x y
+
+--------------------------------------------------------------------------------`
+
+-- Exercise 2
+
+evalStr :: String -> Maybe Integer
+evalStr = liftM eval . parseExp Lit Add Mul
