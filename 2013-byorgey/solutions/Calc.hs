9eb88ee142c1e06a66cb9ee57cc95f1a1031c472
Amar Shah <amar47shah@gmail.com>
2015-10-03 11:46:38 -0400

    Solve Homework 5, Exercise 4

--- 2013-byorgey/solutions/Calc.hs
+++ 2013-byorgey/solutions/Calc.hs
@@ -36,3 +36,34 @@ instance Expr ExprT where
   lit = Lit
   add = Add
   mul = Mul
+
+--------------------------------------------------------------------------------`
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+-- Exercise 4
+
+instance Expr Integer where
+  lit = id
+  add = (+)
+  mul = (*)
+
+instance Expr Bool where
+  lit = (> 0)
+  add = (||)
+  mul = (&&)
+
+newtype MinMax = MinMax Integer deriving (Eq, Ord, Show)
+
+instance Expr MinMax where
+  lit = MinMax
+  add = max
+  mul = min
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr Mod7 where
+  lit = Mod7 . (`mod` 7)
+  add (Mod7 x) (Mod7 y) = lit $ x + y
+  mul (Mod7 x) (Mod7 y) = lit $ x * y
