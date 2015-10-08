210b5246cafe843de1639545be9f4bd8a5082bd3
Amar Shah <amar47shah@gmail.com>
2015-10-08 00:50:31 -0400

    Solve Homework 7, Exercise 1

---
+++ 2013-byorgey/solutions/JoinList.hs
@@ -0,0 +1,20 @@
+module JoinList where
+
+import Data.Function (on)
+import Data.Monoid ((<>))
+
+data JoinList m a = Empty
+                  | Single m a
+                  | Append m (JoinList m a) (JoinList m a)
+                  deriving (Eq, Show)
+
+--------------------------------------------------------------------------------
+
+-- Exercise 1
+(+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
+j +++ k = Append (tag j <> tag k) j k
+
+tag :: Monoid m => JoinList m a -> m
+tag Empty          = mempty
+tag (Single t _)   = t
+tag (Append t _ _) = t
