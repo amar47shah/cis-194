252d7c76368412d822a10d78b47e8926295e1401
Amar Shah <amar47shah@gmail.com>
2015-10-09 22:18:56 -0400

    Solve Homework 7, Exercise 2, Part 1

--- 2013-byorgey/solutions/JoinList.hs
+++ 2013-byorgey/solutions/JoinList.hs
@@ -1,6 +1,8 @@
 {-# OPTIONS_GHC -Wall #-}
 module JoinList where
 
+import Sized
+
 import Data.Monoid ((<>))
 
 data JoinList m a = Empty
@@ -11,6 +13,7 @@ data JoinList m a = Empty
 --------------------------------------------------------------------------------
 
 -- Exercise 1
+
 (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
 j +++ k = Append (tag j <> tag k) j k
 
@@ -18,3 +21,32 @@ tag :: Monoid m => JoinList m a -> m
 tag Empty          = mempty
 tag (Single t _)   = t
 tag (Append t _ _) = t
+
+--------------------------------------------------------------------------------
+
+(!!?) :: [a] -> Int -> Maybe a
+[] !!? _         = Nothing
+_  !!? i | i < 0 = Nothing
+(x:_)  !!? 0     = Just x
+(_:xs) !!? i     = xs !!? (i-1)
+
+jlToList :: JoinList m a -> [a]
+jlToList Empty            = []
+jlToList (Single _ a)     = [a]
+jlToList (Append _ l1 l2) = jlToList l1 ++ jlToList l2
+
+testIndexJ :: Int -> JoinList Size Char -> Bool
+testIndexJ i jl = indexJ i jl == (jlToList jl !!? i)
+
+-- Exercise 2
+
+indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
+indexJ _ Empty                       = Nothing
+indexJ i _              | i < 0      = Nothing
+indexJ i (Single _ x)   | 0 < i      = Nothing
+                        | otherwise  = Just x
+indexJ i (Append t l r) | overC >= 0 = Nothing
+                        | overL <  0 = indexJ i     l
+                        | otherwise  = indexJ overL r
+  where overC = i - (getSize . size) t
+        overL = i - (getSize . size . tag) l
