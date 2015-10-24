8236216b5b64bead10a8b3c1f94eca95107580fd
Amar Shah <amar47shah@gmail.com>
2015-10-24 16:01:40 -0400

    Solve Homework 8, Exercise 3

--- 2013-byorgey/solutions/Party.hs
+++ 2013-byorgey/solutions/Party.hs
@@ -5,6 +5,7 @@ module Party where
 
 import Employee
 
+import Control.Arrow ((&&&))
 import Data.List (foldl')
 import Data.Monoid ((<>))
 import Data.Tree
@@ -40,3 +41,14 @@ treeFold' :: (a -> b) -> (b -> b -> b) -> Tree a -> b
 treeFold' f bop (Node rl ts) = foldl' accumulate initial ts
   where accumulate folded t = folded `bop` treeFold' f bop t
         initial = f rl
+
+--------------------------------------------------------------------------------
+
+-- Exercise 3
+
+-- We get a boss and subdivision guestlists with and without the managers
+-- If we invite the boss, don't invite the subdivision managers, and vice versa
+-- Return one guestlist with the boss and one without
+
+nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel boss = glCons boss . mconcat . map snd &&& mconcat . map fst
